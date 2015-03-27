525f39843bc5e9c0d40e8181ebb818d5bafd04a6
MihaiMijea <mijeamihai@yahoo.com>
2015-03-27 07:59:46 +0100

    add new task input and checkbox but not working

--- todos.js
+++ todos.js
@@ -1,28 +1,28 @@
+
+Tasks = new Mongo.Collection('tasks');
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks:[
-      {text: "task1"},
-      {text: "task2"}
-    ]
+    tasks:function(){
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   })
 
-
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click paper-button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.body.events({
+    "submit .new-task": function(event){
+      var text = event.target.text.value;
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      event.target.text.value = '';
+      return false;
+    },
+    checkboxCK: function(){
+      alert('checked');
     }
   });
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
